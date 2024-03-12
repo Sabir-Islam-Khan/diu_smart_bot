c0530e52ad3b8df4ac9961f75ecc56ceca9e52dc
Sabir Islam Khan <Sabir.islam14@gmail.com>
2024-03-13 00:38:33 +0600

    chunk created

--- helper.py
+++ helper.py
@@ -2,10 +2,15 @@ from langchain.llms import OpenAI
 from dotenv import load_dotenv
 from langchain.document_loaders import UnstructuredURLLoader
 import sitemap as sitemap
+from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
 
 loaders = UnstructuredURLLoader(urls=sitemap.Urls)
 data = loaders.load()
 
-print(data)
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1500, chunk_overlap=200)
+
+docs = text_splitter.split_documents(data)
+
+print(docs)
