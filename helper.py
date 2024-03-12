e921fc16cfef447747b7cbb957002ced282b9229
Sabir Islam Khan <Sabir.islam14@gmail.com>
2024-03-13 00:03:36 +0600

    base change

--- helper.py
+++ helper.py
@@ -1,20 +1,10 @@
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-
+from langchain.document_loaders import UnstructuredURLLoader
+import sitemap as sitemap
 
 load_dotenv()
 
-def detectEmotion(text, lang):
-    llm = OpenAI(temperature=0.5)
-    promptTemplate = PromptTemplate(
-        input_variables=["emotion"],
-        template="""Tell me a romantic caption for my Facebook post. Caption type is {text} and language is {lang}. Return only the caption. Nothing else should be included"""
-    )
-    
-    emotion_chain = LLMChain(llm=llm, prompt=promptTemplate)
-    
-    response = emotion_chain({"text": text, "lang": lang})
-    
-    return response
+loaders = UnstructuredURLLoader(urls=sitemap.Urls)
+data = loaders.load()
+
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import helper as helper
-
+import sitemap as sitemap
 def main():
     st.title("Romantic Caption Generator")
-    
+
     text_input = st.text_input("Enter the text for the caption:")
+    
     lang_input = st.text_input("Enter the language for the caption (e.g., en for English):")
     
     if st.button("Generate Caption"):
         if text_input.strip() and lang_input.strip():
-            caption = helper.detectEmotion(text_input, lang_input)
+            caption = "Your caption will be generated here."
             st.write("Generated Caption:", caption)
         else:
             st.warning("Please enter text and language.")
