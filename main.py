d7ac5040f4b066555b415d80625bd472d8cf39a7
Sabir Islam Khan <Sabir.islam14@gmail.com>
2024-03-12 21:33:05 +0600

    helper seperated

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI
-from dotenv import load_dotenv
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-
-load_dotenv()
-
-def detectEmotion(text, lang):
-    llm = OpenAI(temperature=0.5)
-    promptTemplate = PromptTemplate(
-        input_variables=["emotion"],
-        template="""Tell me a romantic caption for my Facebook post. Caption type is {text} and language is {lang}. Return only the caption. Nothing else should be included"""
-    )
-    
-    emotion_chain = LLMChain(llm=llm, prompt=promptTemplate)
-    
-    response = emotion_chain({"text": text, "lang": lang})
-    
-    return response
+import helper as helper
 
 def main():
     st.title("Romantic Caption Generator")
@@ -27,7 +9,7 @@ def main():
     
     if st.button("Generate Caption"):
         if text_input.strip() and lang_input.strip():
-            caption = detectEmotion(text_input, lang_input)
+            caption = helper.detectEmotion(text_input, lang_input)
             st.write("Generated Caption:", caption)
         else:
             st.warning("Please enter text and language.")
