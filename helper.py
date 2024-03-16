0486e52bee4492b1cf84d4a5b0382ab9cdc43b33
Sabir Islam Khan <Sabir.islam14@gmail.com>
2024-03-17 02:08:44 +0600

    huggingface working

--- helper.py
+++ helper.py
@@ -1,40 +1,54 @@
-from dotenv import load_dotenv
-from langchain.document_loaders import UnstructuredURLLoader
-import sitemap as sitemap
-from langchain.text_splitter import CharacterTextSplitter
-import pickle
-import faiss
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings 
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chains.question_answering import load_qa_chain 
-from langchain import OpenAI 
+from langchain.llms import HuggingFaceHub
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain.vectorstores import FAISS, Chroma
+from langchain.chains import RetrievalQA, LLMChain
+import nest_asyncio
 
-load_dotenv()
-
-loaders = UnstructuredURLLoader(urls=sitemap.Urls)
-data = loaders.load()
-
-text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1500, chunk_overlap=200)
-
-docs = text_splitter.split_documents(data)
-
-embeddings = OpenAIEmbeddings() 
-
-# vectorstore = FAISS.from_documents(docs, embeddings)
-
-# vectorstore.save_local("vectorstore")
-
-x = FAISS.load_local("vectorstore", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
-
-retriever = x.as_retriever()
-llm = OpenAI(temperature=0.0)
-
-chain = RetrievalQAWithSourcesChain(retriever=retriever, combine_documents_chain=load_qa_chain(llm= llm))
+nest_asyncio.apply()
 
 
-def get_information(question):
-    data = chain({"question" : "Answer the questions with context like you are talking to a human. Question is : " + question})
-    return data
+WEBSITE_URL = "https://sabirislam.me/"
 
+loader = WebBaseLoader(WEBSITE_URL)
+loader.requests_per_second = 1
+docs = loader.aload()
 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=20)
+chunks = text_splitter.split_documents(docs)
+
+embeddings = HuggingFaceInferenceAPIEmbeddings(
+    api_key="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", model_name="BAAI/bge-base-en-v1.5"
+)
+
+vectorstore = Chroma.from_documents(chunks, embeddings)
+
+retriever = vectorstore.as_retriever(
+    search_type="mmr", #similarity
+    search_kwargs={'k': 4}
+)
+
+llm = HuggingFaceHub(
+    repo_id="huggingfaceh4/zephyr-7b-alpha",
+    model_kwargs={"temperature": 0.5, "max_length": 64,"max_new_tokens":512}
+)
+
+query = "Who is Sabir Islam Khan?"
+
+prompt = f"""
+ <|system|>
+You are an AI assistant that follows instruction extremely well.
+Please be truthful and give direct answers.  Answer questions only from the context that you are given
+</s>
+ <|user|>
+ {query}
+ </s>
+ <|assistant|>
+"""
+
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="refine", retriever=retriever)
+
+response = qa.run(prompt)
+
+print(response)
