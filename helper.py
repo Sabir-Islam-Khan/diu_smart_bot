9e5065f66d9e955281e4a998abbb157673d2e422
Sabir Islam Khan <Sabir.islam14@gmail.com>
2024-03-17 22:52:56 +0600

    deploy commit

--- helper.py
+++ helper.py
@@ -9,7 +9,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.question_answering import load_qa_chain 
 from langchain import OpenAI 
-
+import streamlit as st
 load_dotenv()
 
 #loaders = UnstructuredURLLoader(urls=sitemap.Urls)
@@ -38,3 +38,23 @@ def get_information(question):
     return data
 
 
+def main():
+    st.title("Daffodil Smart Bot")
+
+    text_input = st.text_input("What do you want to know?")
+    
+    if st.button("Get Information"):
+        if text_input.strip():
+            data = get_information(text_input)
+            if "answer" in data:
+                answer = data["answer"]
+                
+                st.success( answer)
+                
+            else:
+                st.error("No answer found for the query.")
+        else:
+            st.warning("Please enter a question to get an answer.")
+
+if __name__ == "__main__":
+    main()
